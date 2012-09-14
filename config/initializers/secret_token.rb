4e5775efecd18b54b8322d43a98cfa64932c9d3f
Raul Sanchez <raul@um.es>
2012-09-14 16:57:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogCaliandras::Application.config.secret_token = 'aa455c57c5e9114def545e5c0471e2f25cab2d4603087a0b2a47c9c4729cf56d0c72ba32122d7b545bdd9824cb695c10f8165e7905c8741f7a9cb73f0b31ac5a'
